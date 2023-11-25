62aec6414b437f8a6e234b111917b7983679392a
Orvil Alejandro Ibarra Patiño <142275946+Xiclez@users.noreply.github.com>
2023-11-24 21:17:39 -0600

    Update create_db.py

--- data/create_db.py
+++ data/create_db.py
@@ -1,14 +1,6 @@
 from utils.clear_console import clear_console
-from utils.ascii_arts import (
-    draw_pingu,
-    draw_welcome_message,
-    draw_videogames,
-    draw_movies,
-    draw_music,
-    draw_ulsa,
-    draw_tercer_parcial
-)
-from utils.anims.createdb_anim import createdb_anim
+from utils.ascii_arts import draw_pingu
+from utils.ascii_arts import draw_welcome_message
 def create_db():
     print(draw_pingu())
     print(draw_welcome_message())
@@ -16,26 +8,29 @@ def create_db():
     use_default = input("¿Quieres usar la base de datos de Xiklez? (y/n): ").lower()
 
     if use_default == 'n':
-        db_url = input("Ingresa la cadena de conexión a tu base de datos: ")
+        username = input("Ingresa el nombre de usuario de tu base de datos: ")
+        password = input("Ingresa la contraseña de tu base de datos: ")
+        cluster_name = input("Ingresa el nombre de tu clúster de MongoDB Atlas: ")
+        db_url = f"mongodb+srv://{username}:{password}@{cluster_name}.mongodb.net/"
     else:
         db_url = "mongodb+srv://6614:ulsapw2023@ulsa.cpqqpie.mongodb.net/"
+    
     clear_console()
     print("Elige el template para tu base de datos:\n0. Examen tercer parcial:{'CURP','calificacion','materia'}\n1. Videojuegos: {'titulo','clasificacion','genero'}\n2. Música: {'nombre_cancion','Album','genero_musical'}\n3. Películas: {'nombre_peli','director','genero_pelicula'}")
-    schema = input("Elige una opcion (0-3): ")
+    schema = input("Elige una opción (0-3): ")
     clear_console()
-    print("Excelente eleccion! :)")
+    print("Excelente elección! :)")
     if schema == "1":
-        print(draw_videogames())
+        print("Videojuegos")
     elif schema == "2":
-        print(draw_music())
+        print("Música")
     elif schema == "3":
-        print(draw_movies())
+        print("Películas")
     else:
-        print(draw_ulsa())
-        print(draw_tercer_parcial())
-    database = input("¿Cuál será el nombre de tu base de datos? ")
-    collection_name = input("Por último, la colección: ")
-    createdb_anim()
+        print("Ulsa")
+    database = input("¿Cuál será el nombre de tu base de datos? ").strip().replace(" ", "_")
+    collection_name = input("Por último, la colección: ").strip().replace(" ", "_")
 
     return db_url, database, collection_name, schema
+
 
